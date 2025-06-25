9e522ae75abe91996e0c120be24e731263dcbc61
marwaas3d <marwaas3d@gmail.com>
2025-06-25 08:44:05 +0300

    first submit

---
+++ main.js
@@ -0,0 +1,93 @@
+        let shortenBtn = document.getElementById("shorten");
+        let linkinput = document.getElementById("link");
+        let errMsg = document.getElementById("errMsg");
+
+        shortenBtn.addEventListener("click",function(){
+            if(linkinput.value.length <= 0){
+                linkinput.style.border = "3px solid hsl(0, 87%, 67%)"
+                let style = document.createElement("style");
+                style.innerHTML = `
+                input::placeholder {
+                    color: hsl(0, 87%, 67%) !important;
+                }
+                `;
+                document.head.appendChild(style);
+                errMsg.style.display = "block"
+            }
+        })
+/*********************************************FETCH API*************************************** */
+let results = document.getElementById("results");
+
+
+shortenBtn.addEventListener("click", function(){
+    if(linkinput.value.trim().length > 0){
+        const longUrl = linkinput.value.trim();
+        const encodedUrl = encodeURIComponent(longUrl);
+
+  fetch(`https://is.gd/create.php?format=json&url=${encodedUrl}`)
+    .then(res => res.json())
+    .then(data => {
+         console.log(data)
+
+         if(data.shorturl){
+            shortenBtn.setAttribute("disabled","disabled")
+            linkinput.addEventListener("input", function() {
+              if (linkinput.value.trim().length === 0) {
+                  shortenBtn.removeAttribute("disabled");
+                }})
+            
+
+            let resultDiv = document.createElement("div");
+            resultDiv.setAttribute("class","result p-3 rounded-2 mb-3");
+            resultDiv.style.backgroundColor="white";
+
+            let insideDiv = document.createElement("div");
+            insideDiv.setAttribute("class","d-flex flex-column flex-md-row justify-content-between align-items-center")
+
+            resultDiv.appendChild(insideDiv);
+            
+            let pOfInsideDiv = document.createElement("p");
+            pOfInsideDiv.setAttribute("class","mb-0 fw-semibold text-break")
+            pOfInsideDiv.innerHTML = `${linkinput.value.trim()}`
+
+            insideDiv.appendChild(pOfInsideDiv)
+
+            let hr = document.createElement("hr");
+            hr.setAttribute("class","d-block d-md-none w-100")
+            insideDiv.appendChild(hr)
+
+
+            let insideSeconDiv = document.createElement("div")
+            insideSeconDiv.setAttribute("class","parent-class d-flex gap-4 align-items-center flex-column flex-md-row")
+
+            insideDiv.appendChild(insideSeconDiv);
+
+            let span = document.createElement("span");
+            span.setAttribute("class","link fw-semibold w-100")
+            span.innerHTML = `${data.shorturl}`
+
+            insideSeconDiv.appendChild(span)
+
+            let button = document.createElement("button");
+            button.setAttribute("class","btn rounded-2 copy text-white fw-bold px-3 py-2")
+            button.setAttribute("id","copy")
+            button.innerHTML = "Copy";
+
+            insideSeconDiv.appendChild(button)
+
+            results.appendChild(resultDiv)
+
+
+
+            let copy = document.getElementById("copy");
+            copy.addEventListener("click",function(){
+                navigator.clipboard.writeText(span.innerHTML)
+                console.log("copied")
+                copy.style.setProperty("background-color", "hsl(257, 27%, 26%)", "important");
+                copy.innerHTML = "Copied!"
+            })
+         }
+    })
+    }  
+
+})
